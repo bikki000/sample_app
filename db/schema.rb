67a7a0e505ab5f3e415d432d4f17d412414d6417
bikki000 <tnvr.ah.kh@gmail.com>
2018-10-18 11:31:09 +0530

    added date_of_birth and age column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181010052919) do
+ActiveRecord::Schema.define(version: 20181017124619) do
 
   create_table "microposts", force: :cascade do |t|
     t.string   "content"
@@ -36,11 +36,13 @@ ActiveRecord::Schema.define(version: 20181010052919) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "password_digest"
     t.string   "remember_token"
     t.boolean  "admin",           default: false
+    t.date     "date_of_birth",   default: '2018-10-17'
+    t.integer  "age",             default: 0
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
