44ca7b4f9e21c35dbf53b18add2990e2e7ee2e39
bikki000 <tnvr.ah.kh@gmail.com>
2018-10-18 15:06:29 +0530

    added comments in models

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,5 +1,9 @@
 class Relationship < ActiveRecord::Base
 	belongs_to :follower, class_name: "User"
+	# ':follower'=> rails will create a relation with 'Follower' Model [CONVEN]
+	# also provides a handy method to retrive its supertable using name 'follower'
+	# 'class_name "User'=> rails will now create relation with 'User' model [CONFIG]
+	# also the handy methods will named after 'User'
 	belongs_to :followed, class_name: "User"
 
 	validates :follower_id, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,65 @@
 class User < ActiveRecord::Base
 	has_many :microposts, dependent: :destroy
+	# 'has_many :microposts' => rails will look for 'Micropost' model [CONVEN]
+
+	# also rails will look for 'user_id' column in 'microposts' table [CONVEN]
+
+	# 'dependent: :destroy' => rails will delete all microposts of this user
+	# when this user is deleted [CONVEN]
 
 	has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+	# ':relationships'=> rails will look for 'Relationship' model [CONVEN]
+
+	# also rails will look for 'user_id' column in 'relationships' table [CONVEN]
+
+	# 'foreing_key: "follower_id"'=> rails will now look 'follower_id' column
+	# for reference not 'user_id' [CONFIG]
 	has_many :followed_users, through: :relationships, source: :followed
-	# meaning I will user followed_users but you(Rails) will user followed
-	# that will make followed_id instead of followed_user_id
-	# becaus I wanted to use user.followed_users instead of user.followeds
+	# ':followed_users'=> rails will look for 'FollowedUser' Model [CONVEN]
+
+	# also rails will look for 'user_id' column in 'followed_users' table [CONVEN]
+
+	# 'through: :relationships'=> rails will now look for 'Relationship' model [CONFIG]
+
+	# also rails will now look for 'followed_user_id' in relationships table [CONVEN]
+
+	# also provides handy methods named no 'followed_users'
+
+	# 'source: :followed' => rails will now look for followed_id in relationships
+	# table [CONFIG]
+
+	# senario=> we could use the following
+	# 	has_many :followeds, through: :relationships
+	# here will user 'Relationship' table and 'followed_id' foreign key
+	# but this butcher English Grammer when we'll use User.first.followeds
 
 	has_many :reverse_relationships, foreign_key: "followed_id",
 																	 class_name: "Relationship",
 																	 dependent: :destroy
+	# ':reverse_relationships'=> rails will look for 'ReverseRelationship' model [CONVEN]
+
+	# also rails will look for 'user_id' column in 'reverse_relationships' table [CONVEN]
+
+	# 'foreign_key: "followed_id'=> rails will now look for 'followed_id' column
+	# in 'reverse_relationships' table [CONFIG]
+
+	# 'class_name: "Relationship'=> rails now look for 'Relationship' model
+	# not 'ReveseRelationship' model and [CONFIG]
+
+	# also rails will look for 'relationships' table not 'reverse_relationships' table
+	# because now model is 'Relationship' [CONFIG]
 	has_many :followers, through: :reverse_relationships, source: :follower
+	# ':followers'=> rails will look for 'Followew' model [CONVEN]
+
+	# also rails will look for 'user_id' column in 'followers' table [CONVEN]
+
+	# 'through: :reverse_relationships'=> rails now look for 'Relationship' model
+	# (why not 'ReveseRelationships' model) [CONVEN][CONFIG]
+
+	# also rails will look for 'follower_id' column in 'relationships' table
+	# (why not 'reverse_relationships' table) [CONFIG][CONFIG]
+
+	# 'source: :follower'=> this configuration is actually same as convention
 
 	before_save {self.email = email.downcase}
 	before_create :create_remember_token
@@ -24,11 +73,11 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates :password, length: {minimum: 6}
 
-	def User.new_remember_token
+	def self.new_remember_token
 		SecureRandom.urlsafe_base64
 	end
 
-	def User.digest(token)
+	def self.digest(token)
 		Digest::SHA1.hexdigest(token.to_s)
 	end
 
@@ -37,10 +86,14 @@ class User < ActiveRecord::Base
 	end
 
 	def following?(other_user)
+		# I am not using 'followed_user_id' because 'relationships' table doesn't have it
 		relationships.find_by(followed_id: other_user.id)
 	end
 
 	def follow!(other_user)
+		# even though 'relationships' table require follower_id
+		# we are not providing it as rails will do it for us
+		# include 'self' id largly a matter of taste
 		self.relationships.create!(followed_id: other_user.id)
 	end
 
